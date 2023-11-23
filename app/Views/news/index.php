46863a141c5905a7660db149f096ad148b0c202b
Riyan042 <riyandarmawan042@gmail.com>
2023-11-23 08:12:22 +0700

    perbaikan

--- app/Views/news/index.php
+++ app/Views/news/index.php
@@ -5,26 +5,24 @@
 <div class="container">
     <main class="content">
         <div class="inner">
-            <div class="flow">
-                <a href="#" class="latest-news active" id="latest-news-1">
-                    <img src="/assets/img/news/1.jpg" alt="Mouse">
-                    <div class="title">
-                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ut?</h3>
-                    </div>
-                </a>
-                <a href="#" class="latest-news" id="latest-news-2">
-                    <img src="/assets/img/news/2.jpg" alt="Mouse">
-                    <div class="title">
-                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ut?</h3>
-                    </div>
-                </a>
-                <a href="#" class="latest-news" id="latest-news-3">
-                    <img src="/assets/img/news/3.jpg" alt="Mouse">
-                    <div class="title">
-                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ut?</h3>
-                    </div>
-                </a>
-            </div>
+            <a href="#" class="latest-news active" id="latest-news-1">
+                <img src="/assets/img/news/1.jpg" alt="Mouse">
+                <div class="title">
+                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ut?</h3>
+                </div>
+            </a>
+            <a href="#" class="latest-news" id="latest-news-2">
+                <img src="/assets/img/news/2.jpg" alt="Mouse">
+                <div class="title">
+                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ut?</h3>
+                </div>
+            </a>
+            <a href="#" class="latest-news" id="latest-news-3">
+                <img src="/assets/img/news/3.jpg" alt="Mouse">
+                <div class="title">
+                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ut?</h3>
+                </div>
+            </a>
         </div>
     </main>
 </div>
